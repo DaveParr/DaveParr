0bcad641c3a2354914924786020789b18a1e2283
David Parr <davidrgparr@gmail.com>
2020-07-24 19:23:14 +0100

    first pass script

---
+++ script.R
@@ -0,0 +1,32 @@
+library(gh)
+library(tibble)
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+
+
+tibble::tibble(repos = gh::gh("GET /users/:username/repos", username = "daveparr")) %>%
+  tidyr::hoist(
+    repos,
+    name = "name",
+    fork = "fork",
+    forks_count = "forks_count",
+    stargazers_count = "stargazers_count",
+    watchers_count = "watchers_count"
+  ) -> repos
+
+repos %>%
+  dplyr::filter(fork == FALSE) %>%
+  pivot_longer(cols = ends_with("_count"),
+               names_to = "metric", values_to = "count") %>% 
+  ggplot2::ggplot(aes(
+    x = reorder(name, -count), 
+    y = count)) +
+  ggplot2::geom_col() + 
+  ggplot2::guides(x = guide_axis(angle = 45)) +
+  ggplot2::labs(title = "Stargazers by repo",
+                x = "Repo",
+                y = "Stargazers") +
+  theme_minimal() +
+  facet_wrap(~ metric)
+  
