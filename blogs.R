031efa16cba26e2082f8f8026ebe084252a0eaaa
DaveParr <davidrgparr@gmail.com>
2020-08-05 09:38:49 +0100

    test using action to run devto with secret

---
+++ blogs.R
@@ -0,0 +1,13 @@
+library(dev.to.ol)
+library(tidyverse)
+library(knitr)
+
+dev.to.ol::get_users_articles() %>%
+  dplyr::arrange(dplyr::desc(public_reactions_count)) %>%
+  dplyr::slice(1:5) %>%
+  dplyr::mutate(article = paste0('[', title, '](', url, ')')) %>%
+  dplyr::select(article,
+                public_reactions_count,
+                comments_count,
+                page_views_count) %>% 
+  print()
