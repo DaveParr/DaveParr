5683e1ee144c835a714eca47be6424225303b8fa
Dave Parr <8363743+DaveParr@users.noreply.github.com>
2024-03-04 18:18:02 +0000

    Simplify (#1)
    * see if build even still works
    
    * Migrate to v2 of actions for R setup and Rmd rendering
    
    * Include reference to DaveParr.Info
    
    * Add Rprofile, gitignore, and settings files for renv
    Remove code for fetching dev.to blogs
    Modify grpah to be ordered by count
    
    * Include forcats dependency
    
    * Pin r version
    
    * Formatting
    
    * Add libcurl installation step and update blog link
    
    * Re-build Rmarkdown files
    
    * Include graph update job
    
    * Update the graph
    
    * Re-build Rmarkdown files
    
    ---------
    
    Co-authored-by: DaveParr <DaveParr@users.noreply.github.com>
    Co-authored-by: GitHub Actions <actions@github.com>

--- repos.R
+++ repos.R
@@ -4,32 +4,43 @@ library(tidyr)
 library(dplyr)
 library(ggplot2)
 
-tibble::tibble(repos = gh::gh("GET /users/:username/repos", username = "daveparr")) %>%
+repos <- tibble::tibble(
+  repos = gh::gh("/users/{username}/repos",
+    username = "daveparr",
+    .limit = 100
+  )
+)
+
+repos_to_plot <- repos %>%
   tidyr::hoist(
     repos,
-    name = "name",
+    repo_name = "name",
     fork = "fork",
     forks_count = "forks_count",
     stargazers_count = "stargazers_count",
-    watchers_count = "watchers_count"
-  ) -> repos
-
-repos %>%
+  ) %>%
   dplyr::filter(fork == FALSE) %>%
-  dplyr::filter(forks_count > 0 | stargazers_count > 0 | watchers_count > 0) %>% 
-  tidyr::pivot_longer(cols = ends_with("_count"),
-               names_to = "metric", values_to = "count") %>% 
-  dplyr::mutate(metric = stringr::str_remove(metric, "_count")) %>% 
+  dplyr::filter(forks_count > 0 | stargazers_count > 0) %>%
+  dplyr::arrange(desc(stargazers_count)) %>%
+  tidyr::pivot_longer(
+    cols = ends_with("_count"),
+    names_to = "metric", values_to = "count"
+  ) %>%
+  dplyr::arrange(desc(count))
+
+repos_to_plot %>%
   ggplot2::ggplot(aes(
-    x = name, 
+    x = forcats::fct_reorder(repo_name, count, .desc = TRUE),
     y = count,
-    fill = metric)) +
-  ggplot2::geom_col(position = "dodge") + 
+    fill = metric
+  )) +
+  ggplot2::geom_col(position = "dodge") +
   ggplot2::guides(x = guide_axis(angle = 90)) +
-  ggplot2::labs(x = "Repo",
-                y = "Stargazers",
-                fill = "Metric") +
+  ggplot2::labs(
+    x = "Repo",
+    y = "Stargazers",
+    fill = "Metric"
+  ) +
   ggplot2::theme_minimal()
 
 ggplot2::ggsave("graph.png")
-  
